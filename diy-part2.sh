4e45ee663f93672771aa75a890c96fb9ff63b914
FSXEDX <40245347+FSXEDX@users.noreply.github.com>
2020-10-21 13:20:37 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -8,6 +8,8 @@
 #Lienol-APP
 #git clone https://github.com/Lienol/openwrt-package.git package/lienol
 
+# Modify default IP
+sed -i 's/192.168.1.1/10.77.77.1/g' package/base-files/files/bin/config_generate
 #添加新主题
 rm -rf package/lean/luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
@@ -267,3 +269,19 @@ sed -i 's/Multicast subscription renew/组播订阅续订/g' feeds/luci/applicat
 #sed -i 's/VPN/SERVICES/g' package/lienol/lienol/luci-app-passwall/luasrc/model/cbi/passwall/node_list.lua
 #sed -i 's/vpn/services/g' package/lienol/lienol/luci-app-passwall/luasrc/model/cbi/passwall/rule.lua
 #sed -i 's/VPN/SERVICES/g' package/lienol/lienol/luci-app-passwall/luasrc/model/cbi/passwall/rule.lua
+
+mkdir -p files/root
+pushd files/root
+## Install oh-my-zsh
+# Clone oh-my-zsh repository
+git clone https://github.com/robbyrussell/oh-my-zsh ./.oh-my-zsh
+
+# Install extra plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ./.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ./.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-completions ./.oh-my-zsh/custom/plugins/zsh-completions
+
+# Get .zshrc dotfile
+cp ../../../scripts/.zshrc .
+
+popd
